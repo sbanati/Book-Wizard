963d0d43d0cae68d0e093a6036d57f1cf4696e46
smbanati-IZxCKm <smbanati@gmail.com>
2024-04-23 11:57:09 -0400

    added login user mutation functionality

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -2,11 +2,17 @@
 import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { loginUser } from '../utils/API';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
+
+
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
+
+  const [login, { error }] = useMutation(LOGIN_USER);
+
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
 
@@ -26,18 +32,13 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
+      const { data } = await login({
+        variables: { ...userFormData },
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
-    } catch (err) {
-      console.error(err);
-      setShowAlert(true);
+      Auth.login(data.login.token);
+    } catch (error) {
+      console.error(error);
     }
 
     setUserFormData({
@@ -85,8 +86,14 @@ const LoginForm = () => {
           Submit
         </Button>
       </Form>
+      {error && (
+              <div className="my-3 p-3 bg-danger text-white">
+                {error.message}
+              </div>
+            )}
     </>
   );
 };
 
 export default LoginForm;
+
